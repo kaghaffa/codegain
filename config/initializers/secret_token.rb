7def7e367046dc97298b4ed030600f450df0235a
kaghaffa <kaghaffa@gmail.com>
2013-01-22 21:08:22 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codegain::Application.config.secret_token = '7182f904e1fb5b26108608b7aca0abc265bc26e21fc4bf2bc353e1f7613be860aa17b188ea3d3ccec88a29a2583888d95fa10bd8af1ac06dc2f72d0f80139b64'
