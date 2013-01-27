5671a5d79e8b145ac48d5609d34c6d0a7892fa04
David Yoo <dkyoo10@gmail.com>
2013-01-26 18:06:40 -0800

    Add instructors, pictures

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,23 @@
+$(function(){
+  var sections = $('section');
+  var heights = [];
+
+  for( var i in sections ) {
+    heights.push(sections[i].offsetHeight - 64);
+  }
+
+  $(window).scroll(function() {
+
+    $('.nav li.active').removeClass('active');
+
+    var scroll = $(window).scrollTop();
+    if( scroll <= heights[0] ) {
+      $('.nav li').slice(0, 1).addClass('active');
+    } else if( scroll >= heights[0] && scroll <= heights[0] + heights[1] ) {
+      $('.nav li').slice(1, 2).addClass('active');
+    } else {
+      $('.nav li').slice(-1).addClass('active');
+    }
+  })
+
+});
