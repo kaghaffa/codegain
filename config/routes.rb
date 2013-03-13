7ae40ae01223a99274b3d84cc5be05eaa204239b
kaghaffa <kaghaffa@gmail.com>
2013-03-12 21:17:45 -0700

    fixed emailing, partial completion of user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Codegain::Application.routes.draw do
 
   devise_for :users
 
+  match "/dashboard/download" => "dashboard#download"
+
   resources :messages
   resources :dashboard
 
   root :to => 'home#index'
+
 
 end
---
+++ public/files/MilesPerGallon.java
@@ -0,0 +1,9 @@
+public class MilesPerGallon {
+
+  public static void main (String arguments[]) {
+    String myCar = "My car gets ";
+    int milesPerGallon = 30;
+
+    System.out.println(myCar + 30 + " miles per gallon.");
+  }
+}
